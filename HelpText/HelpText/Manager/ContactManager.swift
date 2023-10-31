693c153b99796500a8904ec57e3cafee122862b2
ujhong7 <>
2023-10-31 23:57:49 +0900

    - LocationManager

--- HelpText/HelpText/Manager/ContactManager.swift
+++ HelpText/HelpText/Manager/ContactManager.swift
@@ -19,14 +19,24 @@ class ContactManager: ObservableObject {
     
     //  연락처 정보를 가져옴
     func fetchContacts(completion: @escaping ([String]) -> Void) {
-        let store = CNContactStore()
-        let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
-        let request = CNContactFetchRequest(keysToFetch: keysToFetch)
+        let store = CNContactStore() // 주소록 데이터를 관리하기 위한 클래스
+        let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor] // 주소록에서 가져올 정보를 지정하는데 사용되는 키의 배열
+        let request = CNContactFetchRequest(keysToFetch: keysToFetch) //  주소록에서 정보를 가져올 때 사용하는 요청을 나타내는 객체
         
-        var contactsInfo: [String] = []
+        var contactsInfo: [String] = [] // 가져온 연락처 정보를 저장하는 문자열 배열
         do {
-            
-        } catch {
+            try store.enumerateContacts(with: request) { (contact, stop) in // 주소록에서 연락처 정보를 가져오는 루프를 시작
+                let firstName = contact.givenName
+                let lastName = contact.familyName
+                
+                if let phoneNumbers = contact.phoneNumbers.first?.value { // 현재 연락처의 전화번호를 가져오는 부분입니다. 만약 연락처에 전화번호가 있다면, phoneNumbers에 해당 정보가 저장
+                    let phoneNumber = phoneNumbers.stringValue
+                    contactsInfo.append("\(firstName) \(lastName): \(phoneNumber)") // 이름, 성, 및 전화번호를 조합하여 contactsInfo 배열에 추가
+                } else {
+                    contactsInfo.append("\(firstName) \(lastName): No phone number")
+                }
+            }
+        } catch { // 만약 어떤 이유로든 주소록 정보를 가져올 수 없는 경우
             print("연락처 정보를 가져올 수 없습니다.")
         }
         
@@ -35,7 +45,24 @@ class ContactManager: ObservableObject {
     
     // SIM 카드 정보를 가져옴
     func requestSIMinfo(completion: @escaping (String) -> Void) {
-        
+        let networkInfo = CTTelephonyNetworkInfo()
+        if let carrier = networkInfo.subscriberCellularProvider {
+            let carrierName = carrier.carrierName ?? "Unknown"
+            let countryCode = carrier.isoCountryCode ?? "Unknown"
+            let mobileCountryCode = carrier.mobileCountryCode ?? "Unknown"
+            let mobileNetworkCode = carrier.mobileNetworkCode ?? "Unknown"
+            
+            let simInfo = """
+            Carrier Name: \(carrierName)
+            ISO Country Code: \(countryCode)
+            Mobile Country Code: \(mobileCountryCode)
+            Mobile Network Code: \(mobileNetworkCode)
+            """
+            
+            completion(simInfo)
+        } else {
+            completion("SIM 카드 정보를 가져올 수 없습니다.")
+        }
     }
     
 }
--- HelpText/HelpText/Manager/LocationManager.swift
+++ HelpText/HelpText/Manager/LocationManager.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    private let locationManager = CLLocationManager() // Core Location 프레임워크에서 제공하는 위치 정보 관리를 위한 클래스
+    
+    @Published var currentLocation: String? // 사용자의 현재 위치 정보를 저장하기 위한 @Published 속성, 변경사항이 발생할 때마다 관련된 SwiftUI 뷰에 알림이 전달 함으로써 뷰가 업데이트
+    
+    // 클래스가 다른 클래스를 상속하거나 프로토콜을 준수할 때, 해당 클래스나 프로토콜의 초기화 메서드를 재정의할 수 있습니다.
+    // 기존의 초기화 동작을 수정하거나 확장하는 데 사용됩니다.
+    // 설정 및 위치 업데이트를 시작하는 작업을 수행하기 위해 init 메서드를 재정의
+    override init() {
+        locationManager.delegate = self
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    // 위치 업데이트가 발생하면 호출되는 메서드
+    // 현재 위치 정보를 가져오고 주소로 변환하는 역할
+    // locationManager -> fetchUserLocation 🔴
+    func fetchUserLocation(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let geocoder = CLGeocoder()
+        
+        if let location = locations.last {
+            geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+                if let error = error {
+                    print("Reverse geocoding error: \(error.localizedDescription)")
+                    return
+                }
+                
+                if let placemark = placemarks?.first {
+                    var address = ""
+                    
+                    if let administrativeArea = placemark.administrativeArea {
+                        address = "\(address) \(administrativeArea)"
+                    }
+                    
+                    if let locality = placemark.locality {
+                        address = "\(address) \(locality)"
+                    }
+                    
+                    if let thoroughfare = placemark.thoroughfare {
+                        address = "\(address) \(thoroughfare)"
+                    }
+                    
+                    if let subThoroughfare = placemark.subThoroughfare {
+                        address = "\(address) \(subThoroughfare)"
+                    }
+                    
+                    self.currentLocation = address
+                }
+            }
+        }
+        
+    }
+    
+}
